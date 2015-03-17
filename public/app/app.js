f0726a22f09cf5375dc2aaa546d014957db25879
nxocorp <nxocorp@gmail.com>
2015-03-17 23:07:48 +0000

    angular demo

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,13 @@
 angular.module('app', []);
 angular.module('app').controller('mainCtrl', function($scope) {
     
-    $scope.test = "AngularJS Is Running!";
+    //$scope.test = "AngularJS Is Running!";
+    $scope.projects = [{
+        title: 'CODECANVAS',
+        description: 'Dynamic Semantic Programming Language'
+    }, {
+        title: 'GRIDSPACE',
+        description: 'Modular Mobile Device Framework'
+    }];
     
 });
